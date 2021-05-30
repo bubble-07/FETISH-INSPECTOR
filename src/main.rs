fbb11fbdbac02363a0d5401d556af5b4c30cb8ae
bubble-07 <spagrab@gmail.com>
2021-05-30 19:19:52 -0400

    Add Cargo.toml and source folder

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![allow(dead_code)]
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(unused_parens)]
+
+extern crate ndarray;
+extern crate ndarray_linalg;
+extern crate pretty_env_logger;
+extern crate fetish_lib;
+#[macro_use] extern crate log;
+
+use ndarray::*;
+use rand::prelude::*;
+use fetish_lib::everything::*;
+
+fn main() {
+    pretty_env_logger::init();
+
+    info!("TODO: do stuff");
+}
+
